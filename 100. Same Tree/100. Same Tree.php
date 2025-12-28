f4ec503e138ac47d23cc4d007626281c2f61dac5
Abdelrhman Essam <89538266+abdelrhman-e-dev@users.noreply.github.com>
2025-12-28 21:40:17 +0200

    Add solution: 100. Same Tree

---
+++ 100. Same Tree/100. Same Tree.php
@@ -0,0 +1,32 @@
+/*
+ * Problem: 100. Same Tree
+ * Difficulty: Easy
+ * Link: https://leetcode.com/problems/same-tree/description/
+ * Language: php
+ * Date: 2025-12-28
+ */
+
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     public $val = null;
+ *     public $left = null;
+ *     public $right = null;
+ *     function __construct($val = 0, $left = null, $right = null) {
+ *         $this->val = $val;
+ *         $this->left = $left;
+ *         $this->right = $right;
+ *     }
+ * }
+ */
+class Solution {
+
+    /**
+     * @param TreeNode $p
+     * @param TreeNode $q
+     * @return Boolean
+     */
+    function isSameTree($p, $q) {
+        
+    }
+}
